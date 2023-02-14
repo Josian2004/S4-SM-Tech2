38a72683a19fbd5e1ea5ef5916de28ac64360eb6
Josian van Efferen <github@josian.nl>
2023-02-14 16:47:03 +0100

    Added API connection

--- app/src/main/java/josianvanefferen/tech2_addressgeocoding/MainActivity.kt
+++ app/src/main/java/josianvanefferen/tech2_addressgeocoding/MainActivity.kt
@@ -3,12 +3,14 @@ package josianvanefferen.tech2_addressgeocoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -16,9 +18,22 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 import josianvanefferen.tech2_addressgeocoding.ui.theme.Tech2AddressGeocodingTheme
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : ComponentActivity() {
+    private var streetName:String = ""
+    private var houseNumber:String = ""
+    private var city:String = ""
+    private var currentCoords:LatLng = LatLng(0.0,0.0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,6 +52,7 @@ class MainActivity : ComponentActivity() {
         ) {
             AppTitle()
             AddressInputFields()
+            GoogleMapCompose()
         }
     }
     
@@ -55,7 +71,9 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun AddressInputFields() {
-        val apiKey = BuildConfig.apiKey
+        var streetNameInput by remember {mutableStateOf(streetName)}
+        var houseNumberInput by remember {mutableStateOf(houseNumber)}
+        var cityInput by remember {mutableStateOf(city)}
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -63,26 +81,86 @@ class MainActivity : ComponentActivity() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Street Name:") },
+                value = streetNameInput,
+                onValueChange = {
+                    streetNameInput = it
+                    streetName = it
+                                },
+                label = { Text("Street Name") },
                 modifier = Modifier.padding(10.dp)
             )
 
             TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("House Number:") },
+                value = houseNumberInput,
+                onValueChange = {
+                    houseNumberInput = it
+                    houseNumber = it
+                                },
+                label = { Text("House Number") },
                 modifier = Modifier.padding(10.dp)
             )
 
             TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("City:") },
+                value = cityInput,
+                onValueChange = {
+                    cityInput = it
+                    city = it
+                                },
+                label = { Text("City") },
                 modifier = Modifier.padding(10.dp)
             )
+            
+            Button(onClick = { requestGeocodingData() }) {
+                Text(text = "Search Address")
+            }
         }
     }
+
+    @Composable
+    fun GoogleMapCompose() {
+        var coords by remember { mutableStateOf(currentCoords) }
+        val onValueChange = { currentCoords:LatLng -> coords = currentCoords}
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(coords, 10f)
+        }
+        GoogleMap(
+            cameraPositionState = cameraPositionState,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Marker(
+                state = MarkerState(position = coords),
+                title = "Your Entered Location"
+            )
+        }
+
+    }
+
+    private fun requestGeocodingData () {
+        val client = OkHttpClient()
+        val apiKey = BuildConfig.MAPS_API_KEY
+        streetName.replace("\\s+".toRegex(), "+")
+        println("https://maps.googleapis.com/maps/api/geocode/json?address=${houseNumber}+${streetName},+${city}&key=INSERT_KEY")
+        val request = Request.Builder()
+            .url("https://maps.googleapis.com/maps/api/geocode/json?address=${houseNumber}+${streetName},+${city}&key=${apiKey}")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) =
+                run {
+                    val responseString = response.body()?.string()
+                    println(responseString)
+                    if (!responseString.isNullOrEmpty()) {
+                        val responseJson = JSONObject(responseString)
+                        val lat = responseJson.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").get("lat") as Double
+                        val lng = responseJson.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").get("lng") as Double
+                        currentCoords = LatLng(lat, lng)
+                        println(currentCoords)
+                    }
+                }
+        })
+    }
+
+
 }
 
